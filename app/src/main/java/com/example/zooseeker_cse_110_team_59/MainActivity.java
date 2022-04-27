647b82111135a5c3f28cfab3445ca464b183ffbb
Seth Reis <sethreis3@gamil.com>
2022-04-26 21:51:26 -0700

    User Story 2 Finished (Until We're Given the Data)

--- app/src/main/java/com/example/zooseeker_cse_110_team_59/MainActivity.java
+++ app/src/main/java/com/example/zooseeker_cse_110_team_59/MainActivity.java
@@ -7,12 +7,18 @@ import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> enteredAnimals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enteredAnimals = new ArrayList<>();
     }
 
     public void onSearchSelectClick(View view) {
@@ -24,12 +30,42 @@ public class MainActivity extends AppCompatActivity {
         String searchBarInput = searchBar.getText().toString();
         searchBar.setText("");
 
-        isValid(searchBarInput);
+        if (isValid(searchBarInput) && isNew(searchBarInput)) {
+            addToLists(searchBarInput);
+            increaseListsCount();
+        }
 
         return searchBarInput;
     }
 
     public boolean isValid(String searchBarInput) {
-        return true;
+        if (searchBarInput.equals("") == false) {
+            return true;
+        } else {
+            Utilities.showAlert(this, "Empty Search Bar!", "Please enter some text into the search bar.");
+            return false;
+        }
     }
+
+    public boolean isNew(String searchBarInput) {
+        return !enteredAnimals.contains(searchBarInput);
+    }
+
+    public void addToLists(String searchBarInput) {
+        enteredAnimals.add(searchBarInput);
+
+        TextView animalsListTextView = findViewById(R.id.animals_list_text_view);
+        String animalsListText = animalsListTextView.getText().toString();
+        animalsListTextView.setText(animalsListText + searchBarInput + "\n");
+    }
+
+    public void increaseListsCount() {
+        TextView listCount = findViewById(R.id.list_count_text_view);
+        listCount.setText(enteredAnimals.size() + "");
+    }
+
+    public ArrayList<String> getEnteredAnimals() {
+        return enteredAnimals;
+    }
+
 }
---
+++ app/src/test/java/com/example/zooseeker_cse_110_team_59/UnitTestsUserStory2.java
@@ -0,0 +1,116 @@
+package com.example.zooseeker_cse_110_team_59;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import android.text.NoCopySpan;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class UnitTestsUserStory2 {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testIsValidOnValid() {
+        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
+
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+            assertTrue(activity.isValid("Test"));
+        });
+    }
+
+    @Test
+    public void testIsValidOnInvalid() {
+        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
+
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+            assertFalse(activity.isValid(""));
+        });
+    }
+
+    @Test
+    public void testListStartsEmpty() {
+        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
+
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+            ArrayList<String> testAgainst = new ArrayList<>();
+            assertEquals(testAgainst, activity.getEnteredAnimals());
+        });
+    }
+
+    @Test
+    public void testAddToEmptyList() {
+        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
+
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+            activity.addToLists("Test 1");
+
+            ArrayList<String> testAgainst = new ArrayList<>(Arrays.asList("Test 1"));
+
+            assertEquals(testAgainst, activity.getEnteredAnimals());
+        });
+    }
+
+    @Test
+    public void testAddToNonEmptyList() {
+        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
+
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+            activity.addToLists("Test 1");
+            activity.addToLists("Test 2");
+
+            ArrayList<String> testAgainst = new ArrayList<>(Arrays.asList("Test 1", "Test 2"));
+
+            assertEquals(testAgainst, activity.getEnteredAnimals());
+        });
+    }
+
+    @Test
+    public void testIncreaseCount() {
+        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
+
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+            TextView listCountTextView = activity.findViewById(R.id.list_count_text_view);
+            int listCountBefore = Integer.valueOf(listCountTextView.getText().toString());
+
+            activity.addToLists("Test 1");
+            activity.increaseListsCount();
+            int listCountAfter = Integer.valueOf(listCountTextView.getText().toString());
+
+            assertEquals(listCountBefore + 1, listCountAfter);
+        });
+    }
+}
