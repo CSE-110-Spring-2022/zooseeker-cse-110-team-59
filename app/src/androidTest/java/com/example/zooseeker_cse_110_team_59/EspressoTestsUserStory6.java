0a2ee56575ed7b1cc1441d02b3cc63bc8784d6a5
armaan-akido <armaan@akidolabs.com>
2022-05-08 15:56:18 -0700

    Espresso tests for user story 6 update

--- app/src/androidTest/java/com/example/zooseeker_cse_110_team_59/EspressoTestsUserStory6.java
+++ app/src/androidTest/java/com/example/zooseeker_cse_110_team_59/EspressoTestsUserStory6.java
@@ -1,21 +1,26 @@
 package com.example.zooseeker_cse_110_team_59;
 
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
 
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import androidx.test.espresso.DataInteraction;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -36,7 +41,7 @@ public class EspressoTestsUserStory6 {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void espressoTestsUserStory6() {
+    public void testDirectionsFirstOpened() {
         ViewInteraction materialAutoCompleteTextView = onView(
                 allOf(withId(R.id.search_bar),
                         childAtPosition(
@@ -127,10 +132,145 @@ public class EspressoTestsUserStory6 {
                 allOf(withId(R.id.directions_text), withText("1. Proceed on Entrance Way 10.0 ft towards Africa Rocks Street.\n2. Proceed on Africa Rocks Street 200.0 ft towards Gorillas.\n"),
                         withParent(withParent(withId(android.R.id.content))),
                         isDisplayed()));
-        textView2.check(matches(withText("1. Proceed on Entrance Way 10.0 ft towards Africa Rocks Street. 2. Proceed on Africa Rocks Street 200.0 ft towards Gorillas. ")));
+        textView2.check(matches(withText("1. Proceed on Entrance Way 10.0 ft towards Africa Rocks Street.\n2. Proceed on Africa Rocks Street 200.0 ft towards Gorillas.\n")));
 
         ViewInteraction button = onView(
                 allOf(withId(R.id.next_btn), withText("NEXT: LIONS, 200.0FT"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testDirectionsLastPage() {
+        ViewInteraction materialAutoCompleteTextView = onView(
+                allOf(withId(R.id.search_bar),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialAutoCompleteTextView.perform(replaceText("Gori"), closeSoftKeyboard());
+
+        DataInteraction materialTextView = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(0);
+        materialTextView.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.search_select_btn), withText("Select"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction materialAutoCompleteTextView2 = onView(
+                allOf(withId(R.id.search_bar),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialAutoCompleteTextView2.perform(click());
+
+        ViewInteraction materialAutoCompleteTextView3 = onView(
+                allOf(withId(R.id.search_bar),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialAutoCompleteTextView3.perform(click());
+
+        ViewInteraction materialAutoCompleteTextView4 = onView(
+                allOf(withId(R.id.search_bar),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialAutoCompleteTextView4.perform(replaceText("El"), closeSoftKeyboard());
+
+        DataInteraction materialTextView2 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(0);
+        materialTextView2.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.search_select_btn), withText("Select"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.generate_plan_btn), withText("Generate Plan"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                7),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.directions_btn), withText("Directions"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.next_btn), withText("Next: Elephant Odyssey, 400.0ft"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton5.perform(click());
+
+        ViewInteraction materialButton6 = onView(
+                allOf(withId(R.id.next_btn), withText("Next: Entrance and Exit Gate, 510.0ft"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton6.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.current_exhibit), withText("Directions to Entrance and Exit Gate"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView.check(matches(withText("Directions to Entrance and Exit Gate")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.directions_text), withText("1. Proceed on Africa Rocks Street 200.0 ft towards Sharp Teeth Shortcut.\n2. Proceed on Sharp Teeth Shortcut 200.0 ft towards Reptile Road.\n3. Proceed on Reptile Road 100.0 ft towards Entrance Way.\n4. Proceed on Entrance Way 10.0 ft towards Entrance and Exit Gate.\n"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView2.check(matches(withText("1. Proceed on Africa Rocks Street 200.0 ft towards Sharp Teeth Shortcut.\n2. Proceed on Sharp Teeth Shortcut 200.0 ft towards Reptile Road.\n3. Proceed on Reptile Road 100.0 ft towards Entrance Way.\n4. Proceed on Entrance Way 10.0 ft towards Entrance and Exit Gate.\n")));
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.finish_btn), withText("FINISH"),
                         withParent(withParent(withId(android.R.id.content))),
                         isDisplayed()));
         button.check(matches(isDisplayed()));
